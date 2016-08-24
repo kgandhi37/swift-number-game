a4042bff6d91ccbd76e0ade1e372d8c179a5f7e1
Kishen Gandhi <kishengandhi@hgslapwsad01605.local>
2016-08-24 11:25:07 +0100

    Initial Commit

---
+++ number-game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  number-game
+//
+//  Created by Kishen Gandhi on 24/08/2016.
+//  Copyright © 2016 Kishen Gandhi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
