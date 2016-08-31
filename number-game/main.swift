24ef394ba379c83004976441335f966b00d34a3b
Kishen Gandhi <kishengandhi@hgslapwsad01605.hogarthww.prv>
2016-08-31 17:33:57 +0100

    First Commit

--- number-game/main.swift
+++ number-game/main.swift
@@ -8,5 +8,32 @@
 
 import Foundation
 
-print("Hello, World!")
+let answer = randomIntBetween(0, high: 100) // function called in the helper file
+var turn = 1 // turn as a variable
 
+while (true) { // while loop to allow rounds
+    print("Guess # \(turn): Please enter a number between 1 and 100...")
+    
+    let userInput = input()
+    
+    if let guess = Int(userInput) {
+        if (guess > answer){
+            print("Too high! Lower.")
+        } else if (guess < answer) {
+            print("Too low! Higher")
+        } else {
+            print("Correct!")
+            break // ending the loop
+        }
+    } else {
+        print("Invalid Input") // if invalid input
+        continue // continue loop
+    }
+    turn = turn + 1 //incrementing number of turns
+}
+
+print("It took \(turn) attempts")
+
+debugPrint("It took \(turn) attempts")
+
+NSLog("Program succesfully completed")
